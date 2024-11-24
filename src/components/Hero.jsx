1ba86a9409122b5e6a637f4178c040bcfb178ec2
Lovepreet013 <singhlovepreet1209@gmail.com>
2024-11-24 16:43:32 +0530

    added annotations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import myImage from "../assets/portfolioImg.webp";
+import { RoughNotation} from 'react-rough-notation';
 
 const Hero = () => {
   return (
     <main className="max-w-5xl mx-auto flex flex-col md:flex-row mt-16 items-center md:items-start space-y-8 md:space-y-0" id="about">
       <div className="p-4 md:pr-10 flex-1 text-left md:text-left">
-        <h1 className="text-3xl md:text-5xl font-semibold">
-          Hello! I'm Lovepreet, a developer based in India.
+
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Hello! I'm Lovepreet, a
+          <RoughNotation type="underline" color='#80ADF6' show={true} iterations={3} strokeWidth={2} padding={[1,1,1,1]} >
+           {` developer `}
+          </RoughNotation>
+          based in India.
         </h1>
         <div className="text-lg space-y-4 mt-5">
-          <p>I love building tools that are user-friendly, simple, and delightful.</p>
-          <p>
-            I am a Mechanical Engineering student who has fallen in love with Frontend Development. I have experience developing innovative web applications using technologies such as JavaScript, React.js, and Next.js.
+          <p>I love building tools that are
+            <RoughNotation type="box" color='#d347eb' show={true} iterations={1} strokeWidth={2} padding={[1,1,1,1]} animationDelay={1000}>
+             {` user-friendly, simple and delightful.`}
+             </RoughNotation>
           </p>
-          <p>I'm currently looking for a new role as a developer. Hire me?</p>
+          <p className='sm:w-8/12 md:w-10/12'>
+            I am a Mechanical Engineering student who has fallen in love with
+            <RoughNotation type="highlight" color='#e6aa65' show={true} iterations={1} strokeWidth={3} padding={[1,1,1,1]} animationDelay={1600}>
+             {` Frontend `}
+            </RoughNotation>
+            Development. I have experience developing innovative web applications using technologies such as JavaScript, React.js, and Next.js.
+          </p>
+          <p>I'm currently looking for a new role as a developer.
+            <RoughNotation type="circle" color='#e66572' show={true} iterations={1} strokeWidth={2} padding={[1,1,1,1]} animationDelay={1900}>
+            {` Hire me?`}
+            </RoughNotation>
+          </p>
           <div className="mt-4 space-x-0 md:space-x-4 flex flex-col md:flex-row">
             <button className="px-4 py-2 bg-black text-white hover:bg-slate-700 mb-3 md:mb-0 rounded ease-in-out duration-100">
               <a href="https://www.linkedin.com/in/lovepreetsingh013/" target="_blank" rel="noopener noreferrer">View LinkedIn</a>
